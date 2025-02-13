3de10b9465625af68d84922c1cd2949eb4570d96
Dulguun Baldorj <dulguun.baldorj@mobicom.mn>
2025-02-14 01:05:22 +0800

    gg

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://tenor.com/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "With a chocolate rice cake on top",
+      "What about a matcha frostie",
+      "PLEASE POOKIE",
+      "But :*(",
+      "I am going to die",
+      "Yep im dead",
+      "ok ur talking to Dulguun's ghost",
+      "please babe",
+      ":((((",
+      "PRETTY PLEASE",
+      "Estoy muerto",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+        </>
+      ) : (
+        <>
+          <div
+  className="tenor-gif-embed"
+  data-postid="20341232"
+  data-share-method="host"
+  data-aspect-ratio="0.73125"
+  data-width="15%" // Adjust width as needed
+  style={{ maxWidth: "300px", margin: "auto" }} // Limits size
+>
+  <a href="https://tenor.com/view/bemy-valentine-will-you-be-gif-20341232">
+    Bemy Valentine GIF
+  </a>{" "}
+  from <a href="https://tenor.com/search/bemy-gifs">Bemy GIFs</a>
+</div>
+
+          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
